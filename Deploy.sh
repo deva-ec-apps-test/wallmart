76e7333aea8fb8f49f7c60049ec0f1fa214d685b
deva <kasiviswathreddyb@gmail.com>
2022-02-09 15:56:09 +0530

    users.java file

--- Deploy.sh
+++ Deploy.sh
@@ -1,1 +1,11 @@
+<<<<<<< master
 Hello This is Script file ....
+=======
+ This is used to maufacture the wild Hello This is Script file ....
+<<<<<<< Development
+i  am updating these file to development branch
+=======
+i am update these file:wq!
+>>>>>>> local
+i am ur plance
+>>>>>>> local
